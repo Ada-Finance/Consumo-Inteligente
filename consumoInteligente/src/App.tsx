083f663105351cdc25f8a4f79d3a5758a6aa9802
JacMattisen <jjm_jac@yahoo.com.br>
2026-05-05 18:18:36 +0200

    feat: add initial project structure with React, Tailwind CSS, and Vite
    - Added SVG assets for React and Vite.
    - Implemented Footer component for the application.
    - Created Login component with form handling and navigation.
    - Developed Navbar component with theme toggle and user actions.
    - Set up global styles in index.css with Tailwind CSS.
    - Configured main entry point in main.tsx.
    - Created Dashboard view with Navbar and Footer integration.
    - Added LoginPage view to render the Login component.
    - Configured Tailwind CSS with a custom configuration file.
    - Established TypeScript configuration for the application.
    - Set up Vite configuration for the project.

---
+++ consumoInteligente/src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./views/Dashboard";
+import LoginPage from "./views/LoginPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
